38ea8cc1192093d5106416589382b5fd3324f9b8
qrulleau <quentin.rulleau@yahoo.com>
2023-05-02 09:23:37 +0200

    ADD: reservation route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Reservation from "./pages/Reservation";
 import './App.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
+        <Route path="/reservation" element={<Reservation />} />
       </Routes>
     </Router>
   );
