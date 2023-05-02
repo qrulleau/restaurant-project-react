b387128bda4320391c7448f382d061f399b97fb5
qrulleau <quentin.rulleau@yahoo.com>
2023-05-02 11:13:26 +0200

    ADD: all route for the project

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Reservation from "./pages/Reservation";
+import Menu from "./pages/Menu";
+import News from "./pages/News";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
 import './App.scss';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/reservation" element={<Reservation />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </Router>
   );
