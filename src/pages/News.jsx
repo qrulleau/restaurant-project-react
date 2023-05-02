6a2bf8cd7bc0d3f5937623ad0f0b056a14250ea3
qrulleau <quentin.rulleau@yahoo.com>
2023-05-02 14:59:42 +0200

    ADD: news page done

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header from '../components/header';
+import Article from '../components/article';
+import ThumbnailArticle from '../assets/article/article.jpg';
+import ThumbnailArticleBis from '../assets/article/article1.jpg';
 
 
 function News() {
   return (
-    <div>
+    <div className='news'>
       <Header className="border" background={false} />
+      <div className="content">
+        <h2>News</h2>
+        <div className="d-flex">
+          <Article title="Lorem glavrida nulla dolor ipsum" date="May 3, 2017" imageUrl={ ThumbnailArticle }/>
+          <Article title="New lorem nulla dolor glavrida" date="May 3, 2017" imageUrl={ ThumbnailArticleBis }/>
+          <Article title="New lorem nulla dolor glavrida" date="May 3, 2017" imageUrl={ ThumbnailArticleBis }/>
+          <Article title="New lorem nulla dolor glavrida" date="May 3, 2017" imageUrl={ ThumbnailArticle }/>
+        </div>
+      </div>
     </div>
   );
 }
