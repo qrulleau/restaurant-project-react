d41907a2848ad8caa5736aff33b02a15f8bda039
qrulleau <quentin.rulleau@yahoo.com>
2023-05-02 09:21:09 +0200

    MIN: adjust form component

--- src/components/form/contactForm.jsx
+++ src/components/form/contactForm.jsx
@@ -6,41 +6,41 @@ function ContactForm (){
     <form>
       <h3>Online reservation form</h3>
       <div className="d-flex">
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Name</label>
           <Input type="text" name="name" placeholder="name" />
         </div>
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Surname</label>
           <Input type="text" name="surname" placeholder="surname" />
         </div>
       </div>
       <div className="d-flex">
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Email</label>
           <Input type="email" name="name" placeholder="test@test.fr" />
         </div>
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Phone number</label>
           <Input type="number" name="surname" placeholder="+33 672568440" />
         </div>
       </div>
       <div className="d-flex">
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Number of guests</label>
           <Input type="number" name="surname" placeholder="1" />
         </div>
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Children</label>
           <Input type="number" name="surname" placeholder="0" />
         </div>
       </div>
       <div className="d-flex">
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Time</label>
           <Input type="number" name="time" placeholder="1" />
         </div>
-        <div className="d-flex direction-column">
+        <div className="d-flex direction-column align-start">
           <label htmlFor="">Date</label>
           <Input type="date" name="date" placeholder="10-04-2016"/>
         </div>
@@ -51,7 +51,7 @@ function ContactForm (){
       </div>
       <div className="d-flex justify-start">
         <input type="checkbox" name="" id="" />
-        <p>I have read and agree to the above terms and conditions.</p>
+        <p className="black">I have read and agree to the above terms and conditions.</p>
       </div>
       <input type="submit" value="Submit reservation request" />
     </form>
